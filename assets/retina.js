ead7d01bf45e29bcf1292d85bf10fe1432238456
Janik <janik@kinaj.com>
2014-03-24 13:07:30 +0100

    JS for 2x
    * use js file for retina images
    * support iphone landscape
    * support ipad

---
+++ assets/retina.js
@@ -0,0 +1,21 @@
+// Your stuff ===============================================================
+
+var config = {
+       path: "assets/images/",
+     images: "ios macnn scapple sencha madco cpp",
+  extension: "png"
+},
+
+// ==========================================================================
+
+
+
+files = config.images.split(" "),
+i = files.length
+
+if (devicePixelRatio > 1)
+  while (i--) files[i] += "_2x";
+
+[].forEach.call(document.querySelectorAll("img"), function(el, i) {
+  el.src = config.path + files[i] + "." + config.extension
+})
